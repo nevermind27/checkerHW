7379111ed773c98e22951ef00980baf36f44e2b4
nevermind27 <92424736+nevermind27@users.noreply.github.com>
2023-02-20 22:17:13 +0300

    Add files via upload

---
+++ db/migrate/20211214062210_create_users.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin, default: true
+      t.boolean :superuser, default: true
+
+      t.timestamps
+    end
+  end
+end
